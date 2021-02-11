fe3adfd1952ccaf61fa7ff842bf74bc6320a4629
carlos gonzalez <carlosg266@gmail.com>
2021-02-11 16:07:12 -0500

    migration changes

---
+++ Migrations/ApplicationDb/20210211175742_OrderProductstake3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace crud_app_take2.Migrations.ApplicationDb
+{
+    public partial class OrderProductstake3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderProduct",
+                columns: table => new
+                {
+                    OrderId = table.Column<long>(type: "bigint", nullable: false),
+                    ProductId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProduct", x => new { x.OrderId, x.ProductId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProduct");
+        }
+    }
+}
